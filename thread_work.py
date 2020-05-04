337f9d891bf6a8b07b2aa56acf5d3541368fd380
Jagadeesh Gajula <jagadeesh.jd54@gmail.com>
2020-05-04 10:08:51 +0530

    only extract is working

--- thread_work.py
+++ thread_work.py
@@ -36,18 +36,19 @@ pd.low_memory=False
 df = pd.read_csv("sample_data.csv")
 df.columns=['url','category']
 data = pd.DataFrame(columns=['url','category','text'])
-data.loc[1] = ['url','cat','text']
 avg=[]
 
 
 def stop_words_remover(word_tokens): 
     filtered_sent = [w for w in word_tokens if not w in stop_words] 
     final = ' '.join(filtered_sent)
-    final = ' '.join([i for i in final if not i.isdigit()])
     return final
 
 
-
+def clean_stopwords(text):
+    text_tokens = word_tokenize(text)
+    tokens_without_sw = [word for word in text_tokens if not word in stopwords.words()]
+    return ' '.join(tokens_without_sw)
 
 def extract(url):
     warnings.filterwarnings("ignore")
@@ -55,12 +56,12 @@ def extract(url):
         page = requests.get(url)
         if page.status_code == 200:
             contents = page.content
-            soup = BeautifulSoup(contents,features="lxml")
+            soup = BeautifulSoup(contents)
             [script.decompose() for script in soup(["script", "style"])]
             strips = list(soup.stripped_strings)
             strips = ' '.join(strips)
             strips = strips.replace(',',' ')
-            return stop_words_remover(strips)
+            return clean_stopwords(strips)
         else:
             return "NULL"
     except:
@@ -85,7 +86,6 @@ def extract_link(url):
         s = ' '.join(s)
         s = s.replace('\n',' ')
         s = s.replace(',',' ')
-        s = re.sub('[^A-Za-z0-9]+', ' ',s)
         result.append(s)
     return stop_words_remover(' '.join(result))
 
@@ -113,40 +113,34 @@ def create_df(i,total):
     beg_time = time.time()
     url = prepare_url(df.url[i])
     try:
-        text =  extract_link(url[0])
-        if text == 'NULL':
-            text = extract_link(url[1])
-        if text == 'NULL':
-            text = extract(url[0])
+        text = extract(url[0])
         if text == 'NULL':
             text = extract(url[1])
         data.loc[i] = [df.url[i],df.category[i],text]
     except:
-        time.sleep(.1)
         data.loc[i] = [df.url[i],df.category[i],"NULL"]
     end_time = time.time()
     avg.append(end_time - beg_time)
-    #remaining = total - i 
     load = round(psutil.cpu_percent(),2)
     ram = psutil.virtual_memory()
     ram  = ram.percent
     parallels = threading.activeCount()
-    #remaining_time = round( ((remaining * np.mean(avg))/60)/parallels , 2)
     print("Threads :",parallels,"| avg_life: ",round(np.mean(avg),2),"secs | CPU: ",load,"%  | RAM: ",ram," % |",round((data.shape[0]/total)*100,3)," % complete      ",end='\r')
 
 
 
-begin = int(input("please enter a where to begin: "))
-end =  int(input("please enter where to stop: "))
-name_to_save = str(input("enter a name to save results into, please add .csv at end: "))
-if begin > end: 
-    print("not valid inputs")
-else:
-    format = "%Y-%m-%d %H:%M:%S"
-    now_utc = datetime.now(timezone('GMT'))
-    now_local = now_utc.astimezone(get_localzone())
-    print("Program started at",now_local.strftime(format),"\n\n")
+
+format = "%Y-%m-%d %H:%M:%S"
+now_utc = datetime.now(timezone('GMT'))
+now_local = now_utc.astimezone(get_localzone())
+print("Program started at",now_local.strftime(format),"\n\n")
+
+for j in range(1,int(df.shape[0]/75)):
+    data = pd.DataFrame(columns=['url','category','text'])
+    begin = j * 1000
+    end = begin + 1000
     total = end -  begin
+    print("now on ",j," th part")
     for i in range(begin,end):
         t = threading.Thread(target=create_df,args=(i,total))
         t.start()
@@ -154,18 +148,16 @@ else:
         while threading.activeCount() > 20 or avail < 300 :
             avail = psutil.virtual_memory().available >> 20
             time.sleep(0.1)
-
-
-    print("wait till all threads sync data","." * 30)
-    time.sleep(180)
-    data.to_csv(name_to_save)
-    now_utc = datetime.now(timezone('GMT'))
-    now_local = now_utc.astimezone(get_localzone())
-    print("Program Ended at",now_local.strftime(format),"\n\n")
-    print("data saved if program is still runnning close with CTRL + C.","." * 30)
+    time.sleep(60)
+    data.to_csv("part_"+str(j)+".csv")
+    del(data)
 
 
 
+now_utc = datetime.now(timezone('GMT'))
+now_local = now_utc.astimezone(get_localzone())
+print("Program Ended at",now_local.strftime(format),"\n\n")
+print("data saved if program is still runnning close with CTRL + C.","." * 30)
 
 
 
